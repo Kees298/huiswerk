af91487503fbd611f5441fcf38175e411a28b5da
Kees <k.manshanden@gmail.com>
2024-07-02 11:19:27 +0200

    Comment met huiswerkopdracht

--- src/main/java/nl/Kees298/Main.java
+++ src/main/java/nl/Kees298/Main.java
@@ -1,3 +1,14 @@
+/*
+
+Maak een klein programma dat uit een lijst van getallen telt hoeveel er even of oneven zijn.
+
+Maak een array (int[]) met de volgende getallen: 42, 67, 35, 89, 24, 76, 58, 93, 7, 30, 83, 46, 13, 25, 98, 53, 17, 79, 57, 8.
+Gebruik een for-loop om door de array te itereren.
+Controleer voor elk getal of het even of oneven is met behulp van een if-else-statement.
+Bewaar de telling van even en oneven getallen in een HashMap waarbij de key de string "even" of "oneven" is en de waarde de telling is.
+Letop: de HasMap heeft dus een "key" van het type String en een "value" van het type int.
+
+*/
 package nl.Kees298;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -5,6 +16,7 @@ package nl.Kees298;
 public class Main {
 
   public static void main(String[] args) {
+
 
   }
 }
